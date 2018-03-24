338f2d96fb6cf15620cbeb96c9af5574566c923a
YuniarSetiawan <37091876+YuniarSetiawan@users.noreply.github.com>
2018-03-24 09:47:56 +0700

    Add files via upload

---
+++ Tugas4Proweb/berhasil.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<head>
+        <meta charset="UTF-8">
+        <title>TUGAS 4</title>
+        <link rel="icon"href="assets/images/fto.png">
+</head>
+<body style="background-color: darkkhaki">
+    <center>
+        <img src="assets/images/cut.png" width="150" height="150">
+    </center>
+    <h1 style="text-align: center;color:brown">SELAMAT DATANG</h1>
+    <hr>
+    <h1><marquee style="font-family:verdana">kepuasan anda adalah tujuan kami</marquee></h1>
+    <fieldset  style="float: left">
+        <h2 style="color: red">PELAYANAN SAMSAT</h2>
+        <h3>perpanjangan STNK<h3>
+        <h3>pengesahan STNK<h3>
+        <h3>kendaraan baru<h3
+        <h3>penul 5 tahun<h3>
+    </fieldset>
+    <img src="assets/images/Samsat-Outlet.jpg" width="500" height="300" style="float: left">
+    <img src="assets/images/s.png" width="500" height="300" style="float: left">
+    <h2 style="color: blue">JAM PELAYANAN</h2>
+        <table style="border: 3px black solid">
+            <tr style="background-color: red">
+                <td style="border: 3px black solid"> HARI</td>
+                <td style="border: 3px black solid"> JAM</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> SENIN</td>
+                <td style="border: 3px black solid"> 07.50-15.30</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> SELASA</td>
+                <td style="border: 3px black solid"> 07.50-15.30</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> RABU</td>
+                <td style="border: 3px black solid"> 07.50-15.30</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> KAMIS</td>
+                <td style="border: 3px black solid"> 07.50-15.30</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> JUMAT</td>
+                <td style="border: 3px black solid"> 07.500-11.20</td>
+            </tr>
+            <tr>
+                <td style="border: 3px black solid"> SABTU</td>
+                <td style="border: 3px black solid"> LIBUR</td>
+            </tr>
+        </table>
+    </fieldset>
+    <hr>
+    <footer>
+            <p>copyright 2018 yuniar setiawan</p>
+    </footer>
+</body>
+</html>
